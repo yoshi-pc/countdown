31f8ee9473b8f6deb595f73fb6434bb453b8de35
yoshipc <yosshisan_pc@yahoo.co.jp>
2017-11-23 18:29:27 +0900

    カウントダウン実装

--- script/setup.js
+++ script/setup.js
@@ -9,3 +9,19 @@ $(document).on('ready', function(){
     controller: false
   });
 });
+
+$(function() {
+  setInterval(function() {
+    var now = new Date();
+    var goal = new Date(2017,11,9,18,0,0);
+    var diff = goal.getTime() - now.getTime();
+    var left_days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var left_hours = Math.floor((diff - (left_days * 24 * 60 * 60 * 1000 )) / (1000 * 60 * 60));
+    var left_minutes = Math.floor((diff - (left_days * 24 * 60 * 60* 1000) - (left_hours * 60 * 60 * 1000)) / (1000 * 60));
+    var left_seconds = Math.floor((diff - (left_days * 24 * 60 * 60* 1000) - (left_hours * 60 * 60 * 1000) - (left_minutes * 60 * 1000)) / 1000);
+    $(".num_days").text(left_days);
+    $(".num_hours").text(left_hours);
+    $(".num_minutes").text(left_minutes);
+    $(".num_seconds").text(left_seconds);
+  },1000);
+});
