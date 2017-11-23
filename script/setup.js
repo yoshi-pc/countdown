73e9290ba29a453e1b0ede561d6f29bfce9e2c1a
yoshipc <yosshisan_pc@yahoo.co.jp>
2017-11-23 15:58:58 +0900

    カルーセル完成
    resolve #1

--- script/setup.js
+++ script/setup.js
@@ -1,3 +1,11 @@
 $(document).on('ready', function(){
-  $('.crosscover').crosscover();
+  $('.crosscover').crosscover({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    interval: 7000,
+    startIndex: 0,
+    autoPlay: true,
+    dotsNav: true,
+    controller: false
+  });
 });
