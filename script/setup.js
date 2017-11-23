a9f9fd82643c915123189e0665ca0a269808eaf8
yoshipc <yosshisan_pc@yahoo.co.jp>
2017-11-23 21:52:33 +0900

    可視性の向上と表示追加

--- script/setup.js
+++ script/setup.js
@@ -17,8 +17,8 @@ $(function() {
     var diff = goal.getTime() - now.getTime();
     var left_days = Math.floor(diff / (1000 * 60 * 60 * 24));
     var left_hours = Math.floor((diff - (left_days * 24 * 60 * 60 * 1000 )) / (1000 * 60 * 60));
-    var left_minutes = Math.floor((diff - (left_days * 24 * 60 * 60* 1000) - (left_hours * 60 * 60 * 1000)) / (1000 * 60));
-    var left_seconds = Math.floor((diff - (left_days * 24 * 60 * 60* 1000) - (left_hours * 60 * 60 * 1000) - (left_minutes * 60 * 1000)) / 1000);
+    var left_minutes = Math.floor((diff - (left_days * 24 * 60 * 60 * 1000) - (left_hours * 60 * 60 * 1000)) / (1000 * 60));
+    var left_seconds = Math.floor((diff - (left_days * 24 * 60 * 60 * 1000) - (left_hours * 60 * 60 * 1000) - (left_minutes * 60 * 1000)) / 1000);
     $(".num_days").text(left_days);
     $(".num_hours").text(left_hours);
     $(".num_minutes").text(left_minutes);
