73fa685cc7f862d184dd158e930608c4a807d630
yoshipc <yosshisan_pc@yahoo.co.jp>
2017-11-23 22:38:48 +0900

    負の数カウントダウンを考慮

--- script/setup.js
+++ script/setup.js
@@ -13,12 +13,32 @@ $(document).on('ready', function(){
 $(function() {
   setInterval(function() {
     var now = new Date();
-    var goal = new Date(2017,11,9,18,0,0);
+    var goal = new Date(2017, 10, 23, 22, 28, 0);
     var diff = goal.getTime() - now.getTime();
+    if (diff < 0) {
+      diff = Math.abs(diff);
+      $("#days, #hours, #minutes, #seconds").css({"border":"solid rgba(0,160,234,.7)"});
+    }
     var left_days = Math.floor(diff / (1000 * 60 * 60 * 24));
     var left_hours = Math.floor((diff - (left_days * 24 * 60 * 60 * 1000 )) / (1000 * 60 * 60));
     var left_minutes = Math.floor((diff - (left_days * 24 * 60 * 60 * 1000) - (left_hours * 60 * 60 * 1000)) / (1000 * 60));
     var left_seconds = Math.floor((diff - (left_days * 24 * 60 * 60 * 1000) - (left_hours * 60 * 60 * 1000) - (left_minutes * 60 * 1000)) / 1000);
+    if (left_days === 0) {
+      $("#days").fadeOut("slow");
+      if (left_hours === 0) {
+        $("#hours").fadeOut("slow");
+        if(left_minutes === 0) {
+          $("#minutes").fadeOut("slow");
+        } else {
+          $("#minutes").fadeIn("slow");
+        }
+      } else {
+        $("#hours").fadeIn("slow");
+      }
+    } else {
+      $("#days").fadeIn("slow");
+    }
+
     $(".num_days").text(left_days);
     $(".num_hours").text(left_hours);
     $(".num_minutes").text(left_minutes);
